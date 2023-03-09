8e7db6156ba3e05afea96feaef2441e1b9a29a59
Ali Ijaz <aliejaz0072@gmail.com>
2023-03-09 17:50:42 +0500

    Media Selection

--- src/Components/Greet.js
+++ src/Components/Greet.js
@@ -5,16 +5,22 @@ function Greet() {
         <img src="./emumbaLogo.png" alt="Image not preview" id="fav-logo" />
         <h1 id="first-title">Emumba</h1>
       </div>
-      <div>
-        <input
-          type="text"
-          id="search-box"
-          placeHolder="Search Git..."
-          required
-        ></input>
-        <button id="search-btn">
-          <li class="fa fa-search"></li>
-        </button>
+      <div class="search-box__login">
+        <form action="https://gist.github.com/" method="GET">
+          <div class="search-form__btn">
+            <input
+              type="search"
+              id="search-box"
+              placeHolder="Search Name..."
+              autoComplete="off"
+              required
+            ></input>
+            <button id="search-btn">
+              <li class="fa fa-search"></li>
+            </button>
+          </div>
+        </form>
+        <button id="login-btn">Login</button>
       </div>
     </div>
   );
