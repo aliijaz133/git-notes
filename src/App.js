ab73b95d5b4601211ef3aafb1c3ca201dee94668
Ali Ijaz <aliejaz0072@gmail.com>
2023-03-10 12:09:52 +0500

    Rename File

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import Greet from "./Components/Greet";
 import "./Components/greet.css";
 import Make from "./Components/Make";
-import login from "./Components/login";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
       <Greet />
-      <login />
+      <Login />
       <Make />
     </div>
   );
