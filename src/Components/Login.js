d339fb750c7b11042cae8c545a73f32c5dd88f7a
Ali Ijaz <aliejaz0072@gmail.com>
2023-03-13 15:55:49 +0500

    Form Updating

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,20 +2,28 @@ import { Button, Input } from "antd";
 import CustomButton from "./CustomButton/CustomButton";
 
 const Login = () => {
+  const getText = document.forms["gitForm"]["userName"].value;
+  const getPwd = document.forms["gitForm"]["userName"].value;
   const loginHandler = () => {
-    console.log("Click");
+    if (getText == "" || getPwd == "") {
+      alert("This field is required.");
+      return false;
+    } else {
+      alert("Succesfully loggedin.");
+      return true;
+    }
   };
 
   return (
     <div className="login-form__visibilty">
-      <form name="git-form">
+      <form name="gitForm">
         <div id="login-form">
           <label id="label-name__login">UserName:</label>
           <input
             type="text"
             placeholder="Enter User Name OR Email"
             autoComplete="off"
-            name="user-name"
+            name="userName"
             id="input-field"
             required
           ></input>
@@ -24,7 +32,7 @@ const Login = () => {
             type="password"
             placeholder="Enter Password"
             autoComplete="off"
-            name="user-pwd"
+            name="userPwd"
             id="input-field"
             required
           ></input>
